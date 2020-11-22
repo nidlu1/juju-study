2b5b6a5f8a76f4ff90a6801cd3b37e53957b3146
My <nidlu1@naver.com>
2020-11-22 12:46:52 +0900

    9.4 travis ci와 aws s3, deploy 연동하기 ~357p

--- src/main/java/com/jojoldu/book/springboot/ProfileController.java
+++ src/main/java/com/jojoldu/book/springboot/ProfileController.java
@@ -15,8 +15,11 @@ public class ProfileController {
 
     @GetMapping("/profile")
     public String profile(){
-        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> profiles = Arrays.asList(env.getActiveProfiles()); //env.getActiveProfiles(): 현재 실행중인 activeprofile을 모두 가져온다. 즉 real, oauth, real-db등이 활성화되어있다면 3개 모두 담겨 있음. 여기서 real, real1, real2는 모두 배포에 사용될 profile이라 이중 하나라도 있으면 그 값을 반환하도록 함. 실제로 이번 무중단배포는 real1과 real2만 사용되나 step2를 다시 사용할 수 있으니 real도 남겨둔다.
         List<String> realProfiles = Arrays.asList("real","real1","real2");
+        String defaultProfile = profiles.isEmpty()? "default": profiles.get(0);
+
+        return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
 
     }
 }
--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                                                                             // antMatchers: 권한관리대상을 지정하는 옵션. Url. Http메소드별로 관리가능. "/"등 지정된 url은 permitall() 옵션을 통해 전체 열람권한을 주었다. "/api/v1/**" 주소를 가진 api는 user 권한을 가진 사람만 가능하도록 했다.
                 .anyRequest().authenticated()
                                     //anyRequest(): 설정된 값들 이외 나머지 URL들을 나타냄. authenticated()을 추가해 나머지 URL은 모두 인증된 사용자들에게만 허용. 즉 로그인 한사람만.
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll() // /profile이 인증없이도 호출할 수 있게 한 제외코드. 10장에서 추가됨.
             .and()
                 .logout()
                     .logoutSuccessUrl("/") // 로그아웃 기능에 대한 여러설정의 진입점. 로그아웃 성공시 /주소로 이동
@@ -36,6 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                             //OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정들을 담당.
                         .userService(customOAuth2UserService); //userService: 소셜 로그인 성공시 후속조치를 진행할 UserService 인터페이스의 구현체를 등록.
                                                                 // 리소스서버(소셜서비스..)에서 사용자 정보를 가져온 상태에서 추가로 진행하고자하는 기능을 명시함.
+
 
     }
 }
